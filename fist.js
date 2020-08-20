ddf7a4fad9cb2c571b9598c89d4a6a5efaf9d468
yangzilong <576817275@qq.com>
2020-08-21 00:30:08 +0800

    http

--- fist.js
+++ fist.js
@@ -1,1 +1,9 @@
-console.log(324);
+const http = require('http')
+const url = require('url')
+http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('content-type', 'text-plain')
+  const reqUrl = req.url
+  const userInfo = url.parse(reqUrl,true).query
+  res.end(userInfo.age)
+}).listen(8888)

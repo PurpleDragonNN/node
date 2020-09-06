5519ad595476b3a89b7baab74061f541f5ede671
yangzilong <576817275@qq.com>
2020-09-06 22:25:19 +0800

    封装路由

--- common/util.js
+++ common/util.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
+const path = require('path')
+const url = require('url')
 module.exports = {
-  mime: (type) => {
+  mime(type){
     return new Promise((resolve, reject) => {
       fs.readFile('./common/mime.json',(err,data) => {
         if (err) {
@@ -11,5 +13,32 @@ module.exports = {
       })
     })
 
+  },
+  routerFn(staticUrl,req,res) {
+    let pathName = req.url==='/' ? '/index.html' :url.parse(req.url).pathname
+    fs.readFile(staticUrl+pathName,async (err, data) => {
+      const router = {
+        '/login': () => {
+          res.end('登录')
+        },
+        '/register': () => {
+          res.end('注册')
+        },
+        '/logout': () => {
+          res.end('退出登录')
+        },
+      }
+      if (!err) {
+        let type = await this.mime(path.extname(pathName))
+        res.writeHead(200, {'content-type': `${type};charset="utf-8"`})
+        res.end(data)
+      } else if (router[pathName]) {
+        res.writeHead(200, {'content-type': `text/html;charset="utf-8"`})
+        router[pathName]()
+      } else {
+        res.statusCode = 404
+        res.end('不存在该页面')
+      }
+    })
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,6 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const url = require('url')
+
 const util = require('../common/util')
 http.createServer((req,res) => {
-  let pathName = req.url==='/' ? '/index.html' :url.parse(req.url).pathname
-  fs.readFile(`./static${pathName}`,async (err, data) => {
-    if (err) {
-      res.end('不存在该页面')
-    }
-    let type = await util.mime(path.extname(pathName))
-    res.writeHead(200, {'content-type': `${type};charset="utf-8"`})
-    res.end(data)
-  })
+  util.routerFn('./static',req,res)
 }).listen(8889)
-const obj ={
-  1: false
-}
-Object.entries()

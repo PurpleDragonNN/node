ede172322f36a50ce69ca6f328c36940930989a1
yangzilong <576817275@qq.com>
2020-09-09 23:57:45 +0800

    jade

--- jade/jade.js
+++ jade/jade.js
@@ -1,12 +1,18 @@
 const fs=require('fs')
 const pug=require('pug');
+const obj = {
+  a: 'a',
+  b: 'b'
+}
 var str = pug.renderFile('./view/jade.pug',{
   pretty: true,
   name:'jade',
-  a:1,
-  b:2
+  obj
 })
 fs.writeFile('./view/jade.html',str, (err,data) => {
-  console.log(err);
+  if (err) {
+    console.log('无法读取，目标文件不存在！1');
+    return
+  }
 })
 console.log(str);
